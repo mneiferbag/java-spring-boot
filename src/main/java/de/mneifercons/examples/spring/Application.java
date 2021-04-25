d64e165a5e5c32fec9589917c88c60901b6e76bc
Markus Neifer <mneiferbag@users.noreply.github.com>
2021-04-25 07:09:08 +0000

    Replace println logging with SLF4J

--- src/main/java/de/mneifercons/examples/spring/Application.java
+++ src/main/java/de/mneifercons/examples/spring/Application.java
@@ -2,6 +2,8 @@ package de.mneifercons.examples.spring;
 
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +13,8 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Application {
 
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -19,15 +23,13 @@ public class Application {
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
+            log.info("Let's inspect the beans provided by Spring Boot:");
 
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
             for (String beanName : beanNames) {
-                System.out.println(beanName);
+                log.info(beanName);
             }
-
         };
     }
-
 }
