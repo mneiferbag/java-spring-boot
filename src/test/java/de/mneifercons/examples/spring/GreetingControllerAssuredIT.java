f9373b1cea3753772c98d800a2ecc2494d34a311
Markus Neifer <mneiferbag@users.noreply.github.com>
2021-05-13 13:06:38 +0000

    Add REST Assured tests

---
+++ src/test/java/de/mneifercons/examples/spring/GreetingControllerAssuredIT.java
@@ -0,0 +1,35 @@
+package de.mneifercons.examples.spring;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.context.WebApplicationContext;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+
+@SpringBootTest
+class GreetingControllerAssuredIT {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        RestAssuredMockMvc.webAppContextSetup(webApplicationContext);
+    }
+
+    @Test
+    void statusCodeOkay() {
+        given()
+            .param("name", "Markus")
+        .when()
+            .get("/greeting")
+        .then()
+            .statusCode(200)
+            .body("content", equalTo("Hello, Markus!"));
+    }
+}
---
+++ src/test/java/de/mneifercons/examples/spring/GreetingControllerAssuredTest.java
@@ -0,0 +1,20 @@
+package de.mneifercons.examples.spring;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.Test;
+
+class GreetingControllerAssuredTest {
+
+    @Test
+    void bodyOkay() {
+        given()
+            .standaloneSetup(new GreetingController())
+        .when()
+            .get("/greeting")
+        .then()
+            .statusCode(200)
+            .body(is(equalTo("{\"id\":1,\"content\":\"Hello, World!\"}")));
+    }
+}
