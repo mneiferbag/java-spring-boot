42d919e1a065e2881c2766452d0a25624787c3ee
Markus Neifer <mneiferbag@users.noreply.github.com>
2022-02-14 14:21:20 +0100

    Add mock MVC test

---
+++ src/test/java/eu/mneifercons/examples/spring/VerbControllerMockMvcTest.java
@@ -0,0 +1,40 @@
+package eu.mneifercons.examples.spring;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.context.WebApplicationContext;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@SpringBootTest
+class VerbControllerMockMvcTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssuredMockMvc.webAppContextSetup(webApplicationContext);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        RestAssuredMockMvc.reset();
+    }
+
+    @Test
+    void getVerbByName() {
+        given()
+                .param("name", "trabajar")
+                .when()
+                .get("/verb")
+                .then()
+                .statusCode(200)
+                .body("name", equalTo("trabajar"));
+    }
+}
